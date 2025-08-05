ef6c7da4d2332ee75a47b5b196791bf6a13323ac
RaihaanAman <raihaan200323@gmail.com>
2025-08-05 07:54:35 +0530

    HackerRank problem solutions upload

---
+++ HackerRank/switchCaseAss.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+int main() {
+     int day;
+
+    while (1) {
+        printf("Enter day number (1-7, 0 to quit): ");
+        scanf("%d", &day);
+
+        if (day == 0)
+            break;
+
+        switch (day) {
+            case 1:
+                printf("The given day %d is Monday\n", day);
+                break;
+            case 2:
+                printf("The given day %d is Tuesday\n", day);
+                break;
+            case 3:
+                printf("The given day %d is Wednesday\n", day);
+                break;
+            case 4:
+                printf("The given day %d is Thursday\n", day);
+                break;
+            case 5:
+                printf("The given day %d is Friday\n", day);
+                break;
+            case 6:
+                printf("The given day %d is Saturday\n", day);
+                break;
+            case 7:
+                printf("The given day %d is Sunday\n", day);
+                break;
+            default:
+                printf("Enter a valid day!!\n");
+                break;
+        }
+    }
+
+    return 0;
+}
---
+++ HackerRank/week1/TimeConversion.c
@@ -0,0 +1,121 @@
+#include <assert.h>
+#include <ctype.h>
+#include <limits.h>
+#include <math.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char* readline();
+
+/*
+ * Complete the 'timeConversion' function below.
+ *
+ * The function is expected to return a STRING.
+ * The function accepts STRING s as parameter.
+ */
+
+/*
+ * To return the string from the function, you should either do static allocation or dynamic allocation
+ *
+ * For example,
+ * char* return_string_using_static_allocation() {
+ *     static char s[] = "static allocation of string";
+ *
+ *     return s;
+ * }
+ *
+ * char* return_string_using_dynamic_allocation() {
+ *     char* s = malloc(100 * sizeof(char));
+ *
+ *     s = "dynamic allocation of string";
+ *
+ *     return s;
+ * }
+ *
+ */
+char* timeConversion(char* s) {
+    static char time_24_Hrs[9];
+    int hour_part = (s[0]-'0') * 10 + (s[1]-'0');
+    
+    if(s[8] == 'A'){
+        if(hour_part == 12)
+            hour_part = 0;
+    }else if(s[8] == 'P'){
+        if(hour_part != 12)
+            hour_part += 12;
+    }
+    
+    sprintf(time_24_Hrs, "%02d:%c%c:%c%c", hour_part, s[3], s[4], s[6], s[7]);
+    return time_24_Hrs;
+}
+
+int main()
+{
+    FILE* fptr = fopen(getenv("OUTPUT_PATH"), "w");
+
+    char* s = readline();
+
+    char* result = timeConversion(s);
+
+    fprintf(fptr, "%s\n", result);
+
+    fclose(fptr);
+
+    return 0;
+}
+
+char* readline() {
+    size_t alloc_length = 1024;
+    size_t data_length = 0;
+
+    char* data = malloc(alloc_length);
+
+    while (true) {
+        char* cursor = data + data_length;
+        char* line = fgets(cursor, alloc_length - data_length, stdin);
+
+        if (!line) {
+            break;
+        }
+
+        data_length += strlen(cursor);
+
+        if (data_length < alloc_length - 1 || data[data_length - 1] == '\n') {
+            break;
+        }
+
+        alloc_length <<= 1;
+
+        data = realloc(data, alloc_length);
+
+        if (!data) {
+            data = '\0';
+
+            break;
+        }
+    }
+
+    if (data[data_length - 1] == '\n') {
+        data[data_length - 1] = '\0';
+
+        data = realloc(data, data_length);
+
+        if (!data) {
+            data = '\0';
+        }
+    } else {
+        data = realloc(data, data_length + 1);
+
+        if (!data) {
+            data = '\0';
+        } else {
+            data[data_length] = '\0';
+        }
+    }
+
+    return data;
+}
